50128561eb840f516f9387e9d307e34b3d3921b8
Ryosuke Hayashi <hayashi@hayashikun.com>
2022-04-16 00:30:42 +0900

    Add actix-web and make server work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    format!("Index")
+}
+
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port = 8080;
+    println!("Running on http://localhost:{}", port);
+    HttpServer::new(|| {
+        App::new().service(index).service(greet)
+    })
+        .bind(("0.0.0.0", port))?
+        .run()
+        .await
 }
