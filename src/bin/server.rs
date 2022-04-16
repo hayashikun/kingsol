75555591813f31b2a2a15bcd75cefae97058648f
Ryosuke Hayashi <hayashi@hayashikun.com>
2022-04-17 00:20:28 +0900

    Use anyhow

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,5 +1,7 @@
+use anyhow::Result;
+
 use kingsol::server;
 
-pub fn main() -> std::io::Result<()> {
+pub fn main() -> Result<()> {
     server::start()
 }
--- src/server.rs
+++ src/server.rs
@@ -1,14 +1,15 @@
 use actix_web::{App, HttpServer, web};
+use anyhow::{Context, Result};
 
 use crate::handler::{index_handler, link_handler};
 
 #[actix_web::main]
-pub async fn start() -> std::io::Result<()> {
+pub async fn start() -> Result<()> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index_handler::index))
             .default_service(web::get().to(link_handler::link))
     });
     println!("Running server on http://localhost:8080");
-    server.bind(("0.0.0.0", 8080))?.run().await
+    server.bind(("0.0.0.0", 8080))?.run().await.context("Failed to start server")
 }
