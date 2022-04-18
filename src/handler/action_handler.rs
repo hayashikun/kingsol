32b1aa97348ec7104a3fccac3e4d5fb9cc0dd85c
Ryosuke Hayashi <hayashi@hayashikun.com>
2022-04-18 23:03:53 +0900

    Add form

--- src/handler/action_handler.rs
+++ src/handler/action_handler.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, HttpResponse, web};
+use actix_web::{get, post, HttpResponse, web};
 use actix_web::http::header;
 use anyhow::Result;
 use r2d2::Pool;
@@ -7,26 +7,26 @@ use serde::Deserialize;
 
 use crate::redis::{get_connection, LINK_KEY_PREFIX};
 
-fn do_add(query: web::Query<AddQuery>, pool: web::Data<Pool<Client>>) -> Result<HttpResponse> {
-    let key = query.key.clone();
-    let value = query.value.clone();
-    if key.is_empty() || key == "!" || value.is_empty() {
+fn do_create(params: web::Form<CreateParams>, pool: web::Data<Pool<Client>>) -> Result<HttpResponse> {
+    let key = params.key.clone();
+    let url = params.url.clone();
+    if key.is_empty() || key == "!" || url.is_empty() {
         return Ok(HttpResponse::BadRequest().finish());
     }
     let mut conn = get_connection(&pool)?;
-    conn.set(format!("{}:{}", LINK_KEY_PREFIX, key), value)?;
-    Ok(HttpResponse::TemporaryRedirect().append_header((header::LOCATION, "/!/list")).finish())
+    conn.set(format!("{}:{}", LINK_KEY_PREFIX, key), url)?;
+    Ok(HttpResponse::SeeOther().append_header((header::LOCATION, "/!/list")).finish())
 }
 
 #[derive(Deserialize)]
-pub struct AddQuery {
+pub struct CreateParams {
     key: String,
-    value: String,
+    url: String,
 }
 
-#[get("/!/add")]
-pub async fn add(query: web::Query<AddQuery>, pool: web::Data<Pool<Client>>) -> HttpResponse {
-    match do_add(query, pool) {
+#[post("/!/create")]
+pub async fn create(params: web::Form<CreateParams>, pool: web::Data<Pool<Client>>) -> HttpResponse {
+    match do_create(params, pool) {
         Ok(r) => r,
         Err(e) => HttpResponse::InternalServerError().body(e.to_string())
     }
@@ -35,12 +35,28 @@ pub async fn add(query: web::Query<AddQuery>, pool: web::Data<Pool<Client>>) -> 
 fn do_list(pool: web::Data<Pool<Client>>) -> Result<HttpResponse> {
     let mut conn = get_connection(&pool)?;
     let keys: Vec<String> = conn.keys(format!("{}:*", LINK_KEY_PREFIX))?;
-    let body = keys
+
+    let mut html = "<html><body>".to_string();
+    html += "<ul>";
+    html += &*keys
         .into_iter()
-        .map(|k| format!("{} -> {}", k.clone(), conn.get(k).unwrap_or("?".to_string())))
+        .map(|k| format!("<li>{} -> {}</li>", k.clone(), conn.get(k).unwrap_or("?".to_string())))
         .collect::<Vec<_>>()
-        .join("\n");
-    Ok(HttpResponse::Ok().body(body))
+        .join("");
+    html += "</ul>";
+    html += "
+    <form class='form-horizontal' method='POST' action='/!/create'>
+        <div class='form-group'>
+            <label for='key'>Key</label>
+            <input type='text' name='key' id='key' class='form-control'>
+            <label for='value'>Value</label>
+            <input type='text' name='url' id='url' class='form-control'>
+            <button type='submit'>Add</button>
+        </div>
+    </form>";
+    html += "</body></html>";
+
+    Ok(HttpResponse::Ok().body(html))
 }
 
 #[get("/!/list")]
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
-use actix_web::{App, HttpServer, web};
+use actix_web::{App, HttpResponse, HttpServer, web};
+use actix_web::http::header;
 use anyhow::{Context, Result};
 
 use crate::handler::{action_handler, link_handler};
@@ -11,7 +12,10 @@ pub async fn start() -> Result<()> {
     let server = HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(redis_pool.clone()))
-            .service(action_handler::add)
+            .route("/", web::to(|| async {
+                HttpResponse::TemporaryRedirect().append_header((header::LOCATION, "/!/list")).finish()
+            }))
+            .service(action_handler::create)
             .service(action_handler::list)
             .default_service(web::get().to(link_handler::link))
     });
