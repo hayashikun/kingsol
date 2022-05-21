69b97b4f9b20c17c80ba3792915221c651194be3
Ryosuke Hayashi <hayashi@hayashikun.com>
2022-05-22 00:12:26 +0900

    add auth middleware

--- src/bin/grpc_web.rs
+++ src/bin/grpc_web.rs
@@ -1,6 +1,7 @@
 use tonic::transport::Server;
 
 use kingsol::grpc_api::GrpcApi;
+use kingsol::grpc_interceptor::GrpcInterceptor;
 use kingsol::kingsol_api::kingsol_api_server::KingsolApiServer;
 use kingsol::redis::create_connection_pool;
 
@@ -9,15 +10,16 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let redis_pool = create_connection_pool("redis://localhost:6379").unwrap();
 
     let addr = "0.0.0.0:8081".parse().unwrap();
-    let api = GrpcApi::new(redis_pool);
-    let api = KingsolApiServer::new(api);
-    let api = tonic_web::config().allow_all_origins().enable(api);
+    let api = GrpcApi::new(redis_pool.clone());
+    let interceptor = GrpcInterceptor::new(redis_pool);
+    let server = KingsolApiServer::with_interceptor(api, interceptor);
+    let service = tonic_web::config().allow_all_origins().enable(server);
 
     println!("Server listening on {}", addr);
 
     Server::builder()
         .accept_http1(true)
-        .add_service(api)
+        .add_service(service)
         .serve(addr)
         .await?;
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
 extern crate actix_web;
 
 mod entity;
-pub mod grpc_api;
-mod grpc_controller;
 pub mod interactor;
-pub mod kingsol_api;
 pub mod redis;
 pub mod redis_repository;
 mod repository;
 mod use_case;
+
+pub mod grpc_api;
+mod grpc_controller;
+pub mod grpc_interceptor;
+pub mod kingsol_api;
+
 pub mod web_controller;
